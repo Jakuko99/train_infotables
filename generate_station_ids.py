fa88f1c3b9093d931f594765a4b1dd0fac1309b0
Jakub Krsko <jakubkrs@outlook.sk>
2025-07-13 22:17:35 +0200

    station id update

--- generate_station_ids.py
+++ generate_station_ids.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
-    with open("rest_infotables/stations.txt", "r", encoding="utf-8") as f:
+    with open("assets/stations.txt", "r", encoding="utf-8") as f:
         enum_values = []
         for line in f:
             line = line.strip().upper().split(">")
             line[1] = line[1].replace(" ", "_").replace("-", "_")
             enum_values.append(f"{line[1]} = {line[0]}")
 
-    with open("rest_infotables/station_ids1.py", "w", encoding="utf-8") as f:
+    with open("station_ids.py", "w", encoding="utf-8") as f:
         f.write("from enum import Enum\n\n\n")
         f.write("class StationIds(Enum):\n")
         f.write("\n\t".join(enum_values))
